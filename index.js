d0caef06f7a91737c2006d1efd345af452157ff8
Justin Rodriguez <justin.r@me.com>
2023-05-07 11:24:13 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const element = document.querySelector("main#main");
+element.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Justin is the champion";
